e08206cd8ebd07f2ea211eba32baac3634759e4a
iranika <7370486+iranika@users.noreply.github.com>
2022-04-22 23:20:43 +0900

    add: first commit.

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Nex-Proto:WIP
+
+データフォルダからファイルを抽出してダウンロードリンク用のHTMLを出力するツール
+
+# How to use
+
+1. 関数を読み込む
+    ```
+    . generate-filepath.ps1
+    ```
+
+2. HTMLを出力する関数を実行
+    ```
+    Generate-ExplorerHTML
+    ```
---
+++ generate-filepath.ps1
@@ -0,0 +1,29 @@
+
+function Generate-ExplorerHTML {
+    param(
+        $path = "./",
+        $ext = ("*.mp3","*.wav")
+    )
+    $filePath = Get-ChildItem $PSScriptRoot -Depth 100 -File -Include $ext | % { (Resolve-Path -Relative $_.FullName) -replace "\\","/" }
+
+    $html_template = @"
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nex-Proto</title>
+</head>
+<body>
+    <h1>Download link</h1>
+    <div id="app">
+        $(
+            ($filePath | % { "<li><a href=`"$($_)`" download=`"$(Split-Path $_ -Leaf)`">$($_)</a></li>" }) -join "`n`t`t"
+        )
+    </div>
+</body>
+</html>
+"@
+    $html_template > explorer.html
+}
